9171ca4a9944daa8b74cc488ca86018070ef9d18
Muluken Bekele <mulebeki9.com@gmail.com>
2026-05-15 22:51:50 +0400

    Create 04_measures_exploration.sql

---
+++ script/04_measures_exploration.sql
@@ -0,0 +1,13 @@
+/*
+==========================================================================
+Measures Exploration (Key Metrics)
+==========================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overaal trends or spot anomalies
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===========================================================================
+*/
+
